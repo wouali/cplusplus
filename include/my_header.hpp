b2dfe9f54cb3f820374972ba552d3fe1f5faa0a7
atrantan <atrantan@gmail.com>
2017-12-27 15:57:57 +0100

    Ajout exemple de projet C++

---
+++ include/my_header.hpp
@@ -0,0 +1,7 @@
+#ifndef MY_HEADER_HPP
+#define MY_HEADER_HPP
+
+
+
+#endif // MY_HEADER_HPP
+
---
+++ source/main.cpp
@@ -0,0 +1,8 @@
+// my first program in C++
+#include <iostream>
+#include <my_header.hpp>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+}
