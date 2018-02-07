c6820655e6dfde5f71b95a8e50ac287f296252a5
eleve <eleve@si-PC100>
2018-02-07 15:02:05 +0100

    j'ai fini exo1

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -3,11 +3,16 @@
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
-double exo1(double rayon)
+void exo1(void)
 {
-	double aire;
-	aire = 3.14 * rayon * rayon;
-		return aire;
+	long a = 2, b = 1, c = 0;
+	while (a + b > 0)
+	{
+		a = a - 2;
+		b = b + 1;
+		c = c + 1;
+
+	}
 }
 
 #endif // MY_HEADER_HPP
