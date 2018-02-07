315f641cea410b00c063373fed21411b269cd5e6
eleve <eleve@si-PC100>
2018-02-07 15:04:54 +0100

    j'ai fini exo2

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -3,7 +3,7 @@
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
-void exo1(void)
+void exo1 (void)
 {
 	long a = 2, b = 1, c = 0;
 	while (a + b > 0)
@@ -11,8 +11,21 @@ void exo1(void)
 		a = a - 2;
 		b = b + 1;
 		c = c + 1;
-
 	}
+}
+
+void exo2(void)
+{
+	long n = 3, fac = 1, i = 1;
+	if (n < 0)
+		while (i <= n)
+		{
+			fac = fac * i;
+			i = i + 1;
+		}
+	else
+		if (n < 0)
+			fac = 0;
 }
 
 #endif // MY_HEADER_HPP
